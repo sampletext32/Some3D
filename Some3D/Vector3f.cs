77e9d4acd779c4a998b54731cb51f662d09bddc7
sampletext32 <sampletext32@bk.ru>
2020-07-07 22:26:05 +0300

    Implement more Vector operators

--- Some3D/Vector3f.cs
+++ Some3D/Vector3f.cs
@@ -33,21 +33,72 @@ namespace Some3D
             to.Z = Z;
         }
 
-        public static Vector3f operator *(Vector3f vec, MatrixF m)
+        public Vector3f MultiplySelf(MatrixF m)
         {
-            Vector3f v = new Vector3f();
+            float tX = X;
+            float tY = Y;
+            float tZ = Z;
 
-            v.X = vec.X * m[0, 0] + vec.Y * m[1, 0] + vec.Z * m[2, 0] + m[3, 0];
-            v.Y = vec.X * m[0, 1] + vec.Y * m[1, 1] + vec.Z * m[2, 1] + m[3, 1];
-            v.Z = vec.X * m[0, 2] + vec.Y * m[1, 2] + vec.Z * m[2, 2] + m[3, 2];
-            float w = vec.X * m[0, 3] + vec.Y * m[1, 3] + vec.Z * m[2, 3] + m[3, 3];
+            X = tX * m[0, 0] + tY * m[1, 0] + tZ * m[2, 0] + m[3, 0];
+            Y = tX * m[0, 1] + tY * m[1, 1] + tZ * m[2, 1] + m[3, 1];
+            Z = tX * m[0, 2] + tY * m[1, 2] + tZ * m[2, 2] + m[3, 2];
+
+            float w = tX * m[0, 3] + tY * m[1, 3] + tZ * m[2, 3] + m[3, 3];
 
             if (Math.Abs(w) > 0.00000001f)
             {
-                v.X /= w;
-                v.Y /= w;
-                v.Z /= w;
+                X /= w;
+                Y /= w;
+                Z /= w;
             }
+
+            return this;
+        }
+
+        public Vector3f AddSelf(Vector3f v)
+        {
+            X += v.X;
+            Y += v.Y;
+            Z += v.Z;
+
+            return this;
+        }
+
+        public Vector3f SubSelf(Vector3f v)
+        {
+            X -= v.X;
+            Y -= v.Y;
+            Z -= v.Z;
+
+            return this;
+        }
+
+        public static Vector3f operator *(Vector3f self, MatrixF m)
+        {
+            Vector3f v = new Vector3f();
+            self.Duplicate(v);
+
+            v.MultiplySelf(m);
+
+            return v;
+        }
+
+        public static Vector3f operator +(Vector3f self, Vector3f vec)
+        {
+            Vector3f v = new Vector3f();
+            self.Duplicate(v);
+
+            v.AddSelf(vec);
+
+            return v;
+        }
+
+        public static Vector3f operator -(Vector3f self, Vector3f vec)
+        {
+            Vector3f v = new Vector3f();
+            self.Duplicate(v);
+
+            v.SubSelf(vec);
 
             return v;
         }
