1cbd8c344043410105a451f559ba783ffba405e0
sampletext32 <sampletext32@bk.ru>
2020-07-09 02:40:36 +0300

    Add SomeMaths

---
+++ Some3D/Utils/SomeMaths.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Some3D.Utils
+{
+    public class SomeMaths
+    {
+        public static Vector3f Cross(Vector3f line1, Vector3f line2)
+        {
+            Vector3f v = new Vector3f();
+            v.X = line1.Y * line2.Z - line1.Z * line2.Y;
+            v.Y = line1.Z * line2.X - line1.X * line2.Z;
+            v.Z = line1.X * line2.Y - line1.Y * line2.X;
+            return v;
+        }
+
+        public static Vector3f Cross(Vector3f v1, Vector3f v2, Vector3f v3)
+        {
+            Vector3f line1 = v2 - v1;
+            Vector3f line2 = v3 - v1;
+            return Cross(line1, line2);
+        }
+    }
+}
