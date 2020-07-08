08866c78fc4be2c8f4e3ada7f27c96e282e7960e
sampletext32 <sampletext32@bk.ru>
2020-07-09 00:16:38 +0300

    Add TranslationMatrix field

--- Some3D/Render/Object3D.cs
+++ Some3D/Render/Object3D.cs
@@ -5,6 +5,24 @@ namespace Some3D.Render
     public class Object3D
     {
         public Vector3f Position { get; private set; }
+
+        private MatrixF _translationMatrix;
+        public MatrixF TranslationMatrix => _translationMatrix;
+
+        public void SetPosition(Vector3f position)
+        {
+            Position = position;
+            CalculateTranslationMatrix();
+        }
+
+        private void CalculateTranslationMatrix()
+        {
+            _translationMatrix.MakeIdentity();
+            _translationMatrix[3, 0] = Position.X;
+            _translationMatrix[3, 1] = Position.Y;
+            _translationMatrix[3, 2] = Position.Z;
+        }
+
         public Vector3f Scale { get; private set; }
         public Vector3f Rotation { get; private set; }
 
